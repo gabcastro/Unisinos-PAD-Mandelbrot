b6384a7e221f5ddd69c2183fd349d74710f83e6a
gabriel <gabrielcastrofernandes@hotmail.com>
2021-06-17 13:32:09 -0300

    criação do consumer; alteração nas variáveis de condição; e plotagem dos pixels

--- main.cpp
+++ main.cpp
@@ -14,15 +14,16 @@ using namespace std;
 #define X_RESN 800          /* x resolution */
 #define Y_RESN 800          /* y resolution */
 #define WORKER_SPACE 80     /* length of the square to compute maldelbrot calc */
-#define NUM_THREADS 4
+#define NUM_THREADS 6       /* producer threads */
 
 /* =================================== */
 /* =========== GLOBAL VARS =========== */
 /* =================================== */
 
 pthread_mutex_t pmutex_wBuffer, pmutex_rBuffer;
-pthread_cond_t pcond;
+pthread_cond_t pcond_compute_mandelbrot, pcond_plot_value;
 
+int count_pop_buffer = 0;
 bool goForward = true;
 
 typedef struct complextype
@@ -53,6 +54,12 @@ int i, j, k;
 Compl z, c;
 float lengthsq, temp;
 
+/* Xlib */
+
+Window win;                        
+Display *display;
+GC gc;
+
 /* =============================== */
 /* =========== METHODS =========== */
 /* =============================== */
@@ -62,11 +69,12 @@ void calculate_maldelbrot(quadrant *w_area_selected);
 void worker_quadrants();
 void *producer(void *str);
 void create_threads();
+void *consumer(void *str);
+void draw_point(map_point *point);
 
 
 int main()
 {
-    Window win;                        /* initialization for a window */
     unsigned int width, height,        /* window size */
         x, y,                          /* window position */
         border_width,                  /*border width in pixels */
@@ -74,10 +82,8 @@ int main()
         screen;                        /* which screen */
 
     char *window_name = "Mandelbrot Set", *display_name = NULL;
-    GC gc;
     unsigned long valuemask = 0;
     XGCValues values;
-    Display *display;
     XSizeHints size_hints;
     Pixmap bitmap;
     XPoint points[800];
@@ -146,35 +152,10 @@ int main()
     XMapWindow(display, win);
     XSync(display, 0);
 
-    /* Calculate and draw points */
-
-    for (i = 0; i < X_RESN; i++)
-        for (j = 0; j < Y_RESN; j++)
-        {
-
-            z.real = z.imag = 0.0;
-            c.real = ((float)j - 400.0) / 200.0; /* scale factors for 800 x 800 window */
-            c.imag = ((float)i - 400.0) / 200.0;
-            k = 0;
-
-            do
-            { /* iterate for pixel color */
-
-                temp = z.real * z.real - z.imag * z.imag + c.real;
-                z.imag = 2.0 * z.real * z.imag + c.imag;
-                z.real = temp;
-                lengthsq = z.real * z.real + z.imag * z.imag;
-                k++;
-
-            } while (lengthsq < 4.0 && k < 20);
-
-            if (k == 20) {
-                XDrawPoint(display, win, gc, j, i);
-            }
-        }
-
     worker_quadrants();
     create_threads();
+
+    cout << "tamanho do buffer com as coordenadas para plotar: " << r_buffer.size() << endl;
 
     XFlush(display);
     sleep(20);
@@ -260,6 +241,9 @@ void *producer(void *str) {
             pthread_mutex_unlock(&pmutex_wBuffer);
             break;
         }
+        
+        cout << "consumindo valor do vetor de posicoes " << endl;
+
         quadrant w_area_selected = w_buffer.at(0);
         w_buffer.erase(w_buffer.begin(), w_buffer.begin()+1);
 
@@ -268,13 +252,40 @@ void *producer(void *str) {
         pthread_mutex_lock(&pmutex_rBuffer);
 
         while (!goForward)
-            pthread_cond_wait(&pcond, &pmutex_rBuffer);
+            pthread_cond_wait(&pcond_compute_mandelbrot, &pmutex_rBuffer);
 
         goForward = false;
+        cout << "realiza o calculo mandelbrot" << endl;
         calculate_maldelbrot(&w_area_selected);
-        goForward = true;        
-        pthread_cond_signal(&pcond);
+        goForward = true;  
+
         pthread_mutex_unlock(&pmutex_rBuffer);
+
+        // sleep(3); // good to watch the prints on terminal
+
+        pthread_cond_signal(&pcond_compute_mandelbrot);
+        pthread_cond_signal(&pcond_plot_value);
+    }
+}
+
+void *consumer(void *str) {
+    while (1) {
+        pthread_mutex_lock(&pmutex_rBuffer);
+
+        while (r_buffer.size() == 0) 
+            pthread_cond_wait(&pcond_plot_value, &pmutex_rBuffer);
+        
+        count_pop_buffer++;
+        cout << count_pop_buffer << " - delete data from buffer result " << endl;
+
+        map_point m_point = r_buffer.at(0);
+        r_buffer.erase(r_buffer.begin(), r_buffer.begin()+1);
+
+        draw_point(&m_point);
+
+        pthread_mutex_unlock(&pmutex_rBuffer);
+
+        pthread_cond_signal(&pcond_compute_mandelbrot);
     }
 }
 
@@ -283,7 +294,8 @@ void create_threads() {
     pthread_mutex_init(&pmutex_wBuffer, NULL);
     pthread_mutex_init(&pmutex_rBuffer, NULL);
 
-    pthread_cond_init(&pcond, NULL);
+    pthread_cond_init(&pcond_compute_mandelbrot, NULL);
+    pthread_cond_init(&pcond_plot_value, NULL);
 
     pthread_t producer_threads[NUM_THREADS];
     pthread_t consumer_thread;
@@ -291,10 +303,14 @@ void create_threads() {
     for (int i = 0; i < NUM_THREADS; i++) {
         pthread_create(&producer_threads[i], NULL, producer, NULL);
     }
-    // pthread_create(&consumer_thread, NULL, consumer, NULL);
+    pthread_create(&consumer_thread, NULL, consumer, NULL);
 
     for (int i = 0; i < NUM_THREADS; i++) {
         pthread_join(producer_threads[i], NULL);
     }
-    // pthread_join(consumer_thread, NULL);
+    pthread_join(consumer_thread, NULL);
+}
+
+void draw_point(map_point *point) {
+    XDrawPoint(display, win, gc, point->column, point->row);
 }
